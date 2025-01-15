17bee3375cb26ceb8cda56c2722d73348252430d
garg-tejas <155092457+garg-tejas@users.noreply.github.com>
2025-01-15 20:19:46 +0530

    Time: 4 ms (29.16%), Space: 8.2 MB (82.68%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -5,16 +5,12 @@ public:
         int l2 = word2.length();
         string s;
         int idx1 = 0, idx2 = 0;
-        for (int i = 0; i <l1+l2; i++) {
-            if (i % 2 == 0 && idx1<l1)
+        for (int i = 0; i < max(l1, l2); i++) {
+            if (idx1 < l1)
                 s += word1[idx1++];
-            else if(idx2<l2)
+            if (idx2 < l2)
                 s += word2[idx2++];
         }
-        while (idx1 < l1)
-            s += word1[idx1++];
-        // while (idx2 < l2)
-        //     s += word2[idx2++];
         return s;
     }
 };
