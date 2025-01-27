80f3b044d04a03772adb9cb93777f03fdbf63210
garg-tejas <155092457+garg-tejas@users.noreply.github.com>
2025-01-27 19:47:57 +0530

    Time: 0 ms (100%), Space: 28.3 MB (24.57%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,16 +3,12 @@ public:
     int majorityElement(vector<int>& nums) {
         unordered_map<int, int> freq;
         int n = nums.size();
-        int maxFreq = 0;
-        int max = -1;
         for (auto& num : nums) {
             freq[num]++;
-            if (freq[num] > maxFreq) {
-                max = num;
-                maxFreq = freq[num];
-                // cout<<max<<" "<<freq[num]<<endl;
+            if (freq[num] > n/2) {
+                return num;
             }
         }
-        return max;
+        return -1;
     }
 };
