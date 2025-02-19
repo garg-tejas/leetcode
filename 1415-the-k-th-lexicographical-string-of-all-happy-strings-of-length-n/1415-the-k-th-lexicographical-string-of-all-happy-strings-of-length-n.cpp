f60266a7b96e0426e4e543b1648ec177d8bf8a68
garg-tejas <155092457+garg-tejas@users.noreply.github.com>
2025-02-19 21:46:08 +0530

    Time: 7 ms (57.82%), Space: 8.4 MB (84.68%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,36 +1,29 @@
 class Solution {
 public:
     string getHappyString(int n, int k) {
-        string res;
+        string res(n, 'a');
         if (k > 3 * pow(2, n - 1))
             return "";
-        if (n == 1) {
-            if (k == 1)
-                return "a";
-            else if (k == 2)
-                return "b";
-            return "c";
-        }
         int p = pow(2, n - 1);
         int startLetter = (k - 1) / p;
         k = k - 1 - (startLetter * p);
         if (startLetter == 0)
-            res += 'a';
+            res[0] = 'a';
         else if (startLetter == 1)
-            res += 'b';
+            res[0] = 'b';
         else
-            res += 'c';
+            res[0] = 'c';
         unordered_map<char, vector<char>> dict;
         dict['a'] = {'b', 'c'};
         dict['b'] = {'a', 'c'};
         dict['c'] = {'a', 'b'};
-        for (int i = 2; i <= n; i++) {
+        for (int i = 1; i < n; i++) {
             p /= 2;
             int idx = k < p ? 0 : 1;
             if (idx == 1)
                 k -= p;
             cout << idx << " " << k << " " << p << endl;
-            res += dict[res.back()][idx];
+            res[i] = dict[res[i-1]][idx];
         }
         return res;
     }
