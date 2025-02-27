d07aa19f48622e44580f871b435fbf318560e3e3
garg-tejas <155092457+garg-tejas@users.noreply.github.com>
2025-02-27 07:45:37 +0530

    Time: 268 ms (80.77%), Space: 15.7 MB (74.57%) - LeetHub

--- 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.cpp
+++ 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.cpp
@@ -6,14 +6,13 @@ public:
         int maxSub = 0;
         for (int i = 0; i < n - 1; i++) {
             for (int j = i + 1; j < n; j++) {
-                int cur = arr[i];
+                int curr = arr[i];
                 int res = 2;
                 int prev = arr[j];
-                cur = cur + prev;
-                while (numSet.find(cur) != numSet.end()) {
-                    int temp = cur;
-                    cur += prev;
-                    prev = temp;
+                curr = curr + prev;
+                while (numSet.count(curr)) {
+                    curr = curr + prev;
+                    prev = curr - prev;
                     maxSub = max(maxSub, ++res);
                 }
             }
