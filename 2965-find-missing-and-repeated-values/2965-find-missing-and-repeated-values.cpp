1a215815cf338ab892bf18bece64f98f7c7ddcfc
garg-tejas <155092457+garg-tejas@users.noreply.github.com>
2025-02-28 23:15:41 +0530

    Time: 0 ms (100%), Space: 25.6 MB (95.05%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
+    static long long sumRow(long long sum, vector<int>& row) {
+        return sum + accumulate(row.begin(), row.end(), 0LL);
+    }
+
+    static long long sumSquareElement(long long sum, int x) {
+        return sum + (long long)x * x;
+    }
+
+    static long long sumSquareRow(long long sum, vector<int>& row) {
+        return sum + accumulate(row.begin(), row.end(), 0LL, sumSquareElement);
+    }
+
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        unordered_map<int, int> freq;
-        vector<int> res;
-        int total=0;
-        for (auto& g : grid) {
-            for (auto& element : g) {
-                freq[element]++;
-                total += element;
-                if (freq[element] == 2) {
-                    res.push_back(element);
-                }
-            }
-        }
-        int n = grid.size();
-        int second = ((n * n + 1) * n * n / 2) - total + res[0];
-        res.push_back(second);
-        return res;
+        long long sqrSum = accumulate(grid.begin(), grid.end(), 0LL, sumSquareRow);
+        long long sum = accumulate(grid.begin(), grid.end(), 0LL, sumRow);
+        long long n = grid.size() * grid.size();
+        long long expSum = n * (n + 1) / 2;
+        long long expSqrSum = n * (n + 1) * (2 * n + 1) / 6;
+        long long sumDiff = sum - expSum;
+        long long sqrDiff = sqrSum - expSqrSum;
+        int repeat = (sqrDiff / sumDiff + sumDiff) / 2;
+        int missing = (sqrDiff / sumDiff - sumDiff) / 2;
+        return {repeat, missing};
     }
 };
