02e0c87849e62f7b6fb6c777717ebccfb9702f8b
garg-tejas <155092457+garg-tejas@users.noreply.github.com>
2025-03-05 07:21:01 +0530

    Time: 0 ms (100%), Space: 7.9 MB (31.17%) - LeetHub

--- 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        if(n==1) return 1;
-        return (n-1)*4 + coloredCells(n-1);
+        long long m = n;
+        return 1 + (m - 1) * (4 + (m - 2) * 2);
     }
 };
